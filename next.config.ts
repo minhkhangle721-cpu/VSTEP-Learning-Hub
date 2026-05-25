f75b488af144f2fed599e2f7077105b25f9e73fa
minhkhangle721-cpu <minhkhangle721@gmail.com>
2026-05-25 20:21:24 +0700

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from "next";
+
+const repoName = 'my-vstep-hub'; // Tên thư mục dự án của b
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig: NextConfig = {
+  output: 'export',          // Bắt buộc để xuất ra html/css tĩnh
+  images: {
+    unoptimized: true,      // Bắt buộc để không bị lỗi thẻ Image
+  },
+  trailingSlash: true,
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+};
+
+export default nextConfig;
