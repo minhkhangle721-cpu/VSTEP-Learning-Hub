3bb71711785dfae9c38ab13009fbb3c87343a5bb
minhkhangle721-cpu <minhkhangle721@gmail.com>
2026-05-29 13:26:54 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-<<<<<<< HEAD
+
 const repoName = 'my-vstep-hub'; // Tên thư mục dự án của b
-=======
+
 const repoName = 'VSTEP-Learning-Hub'; // Tên thư mục dự án của b
->>>>>>> f2fe1cd892b79f0954bb2c46a3ef348af7361644
+
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
@@ -17,11 +17,11 @@ const nextConfig: NextConfig = {
   assetPrefix: isProd ? `/${repoName}/` : '',
 };
 
-<<<<<<< HEAD
+
 export default nextConfig;
-=======
+
 export default nextConfig;
 
 
->>>>>>> f2fe1cd892b79f0954bb2c46a3ef348af7361644
+
 
